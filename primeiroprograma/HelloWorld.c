762c4bf022ca718e0bb82a5e9f5619257a39b566
Lucas Ribeiro <asahisauishas@gmail.com>
2021-03-04 03:09:31 +0000

    .

---
+++ primeiroprograma/HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/** primeiro hello world em C  **/
+
+int main (){
+    printf("Hello World\n"); /** escreve Hello World na tela **/
+    return(0); /** retorna 0 pois executou com sucesso **/
+       }
