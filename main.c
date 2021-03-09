9b83b81825c4128a6cad6824b624bd010d00b3e5
Lucas Ribeiro <lucasribeiro5569@gmail.com>
2021-03-09 05:14:02 +0000

    repl.it

--- main.c
+++ main.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 //* Variaveis em C *//
 
-int main(void){
+int main(void) {
     int a, b;
     float soma;
-    char nome[13];
+    char nome[8];
     printf("digite seu nome: ");
     scanf("%s", nome);
     printf("OK %s, digite um numero: ", nome);
     scanf("%d", &a);
     printf("digite outro numero:");
     scanf("%d", &b);
-    soma =  a + b;
-    printf("a soma de %d + %d = %f", a, b , soma);
-
+    soma = a + b;
+    printf("a soma de %d + %d = %f", a, b, soma);
+    return(0);
 }
--- primeiroprograma/HelloWorld.c
+++ primeiroprograma/HelloWorld.c
@@ -2,7 +2,7 @@
 
 /** primeiro hello world em C  **/
 
-int main (){
+int main() {
     printf("Hello World\n"); /** escreve Hello World na tela **/
-    return(0); /** retorna 0 pois executou com sucesso **/
-       }
+    return (0); /** retorna 0 pois executou com sucesso **/
+}
