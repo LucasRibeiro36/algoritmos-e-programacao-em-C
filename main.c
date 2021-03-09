9069a94e098bcec80c441ab146d395ae8f655981
Lucas Ribeiro <lucasribeiro5569@gmail.com>
2021-03-09 04:09:59 +0000

    um pouco de variáveis em c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+//* Variaveis em C *//
+
+int main(void){
+    int a, b;
+    float soma;
+    char nome[13];
+    printf("digite seu nome: ");
+    scanf("%s", nome);
+    printf("OK %s, digite um numero: ", nome);
+    scanf("%d", &a);
+    printf("digite outro numero:");
+    scanf("%d", &b);
+    soma =  a + b;
+    printf("a soma de %d + %d = %f", a, b , soma);
+
+}
