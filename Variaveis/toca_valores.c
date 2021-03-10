1242ba0819fe9fc509b01826bb6c021e0272ba11
lucasribeiro <lucasribeiro5569@gmail.com>
2021-03-10 13:25:28 -0300

    .

---
+++ Variaveis/toca_valores.c
@@ -0,0 +1,14 @@
+//* Variaveis em C *//
+#include <stdio.h>
+
+int main(){
+    int soma, a, b;
+    a = 1;
+    b = 4;
+    soma = a + b;
+    printf("a soma de %d + %d = %d\n",a,b,soma);
+    a = 5;
+    b = 3;
+    soma = a + b;
+    printf("a soma de %d + %d = %d\n",a,b,soma);
+}
