b4d75f500b8e26450a2eb6894f25d96f95ed5bd9
lucas <lucasribeiro5569@gmail.com>
2021-03-22 14:48:40 -0300

    .

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(void) {
     float nota_aluno;
     printf("Digite sua nota:");
     scanf("%f", &nota_aluno);
-    if (nota_aluno >= 7.0){
+    if (nota_aluno >= 7.0) {
         printf("Voce foi aprovado!!!\n");
-    } else
-    if(nota_aluno >= 3){
-        printf("Voce esta na prova final!!!");
-    }else
-    if (nota_aluno < 3){
+    } else if (nota_aluno >= 3) {
+        printf("Voce esta na prova final!!!\n");
+    } else {
         printf("Nem na final vc conseguiu chegar!!!\n");
     }
 }
