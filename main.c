39058a8853b685be7d4c3cdc0fc44a179a12fc58
lucas <lucasribeiro5569@gmail.com>
2021-03-22 14:42:16 -0300

    .

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-    int metros;
-    float decimetros, centimetros, milimetros;
-    printf("Digite os metros:");
-    scanf("%i", &metros);
-    decimetros = metros * 10; /* multiplica por 10*/
-    centimetros = metros * 100;/* multiplica por 100*/
-    milimetros = metros * 1000;/* multiplica por 1000*/
-    printf("%i metros\n%.2f decimetros \n%.2f centimetros\n%.2f milimetros\n", metros, decimetros, centimetros ,milimetros);
-    system("PAUSE");
-    return(0);
+int main(void) {
+    float nota_aluno;
+    printf("Digite sua nota:");
+    scanf("%f", &nota_aluno);
+    if (nota_aluno >= 7.0){
+        printf("Voce foi aprovado!!!\n");
+    } else
+    if(nota_aluno >= 3){
+        printf("Voce esta na prova final!!!");
+    }else
+    if (nota_aluno < 3){
+        printf("Nem na final vc conseguiu chegar!!!\n");
+    }
 }
