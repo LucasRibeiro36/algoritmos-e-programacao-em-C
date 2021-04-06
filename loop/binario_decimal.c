23a9bf230ee5d729bbe0ded57d77d3f306a75dcd
lucas <lucasribeiro5569@gmail.com>
2021-04-06 08:55:27 -0300

    aaaaaaaaaaaaaaaaaaaaa

--- loop/binario_decimal.c
+++ loop/binario_decimal.c
@@ -1,16 +1,22 @@
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
+//
+// Created by lucas on 06/04/2021.
+//
 
-int main()
-{
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main(){
     char bin[100];
-    int dec = 0;
-    int i = 0;
-    int s;
-    fgets( bin, sizeof(bin), stdin);
+    int decimal,i,s;
+    printf("Digite o numero em binario:");
+    scanf("%s",bin);
     s = strlen( bin );
-    while( s-- ) dec = dec + pow(2, i++) * (bin[s] - 48);
-    printf("\nDecimal Equivalent of Binary Number: \t %d\n", dec);
-    return 0;
+    for (i = s-1; i >= 0; i--) {
+        if (bin[i] == '1') {
+            decimal += (pow(2,s-1-i));
+            printf("%d",decimal);
+        }
+    }
+    printf("\n%d\n",decimal);
+
 }
