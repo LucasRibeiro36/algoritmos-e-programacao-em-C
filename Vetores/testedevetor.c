974fe95fcf30bee081b1810a01796ce2ee2702b8
LucasRibeiro36 <lucasribeiro5569@gmail.com>
2021-05-15 19:30:06 -0300

    threads

--- Vetores/testedevetor.c
+++ Vetores/testedevetor.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 int main(){
-    int vetor[15];
 
-    for (int cont=0;cont<=15;cont++){
-        vetor[cont] = cont*3;
-        printf("%d ",cont*3);
-    }
-
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < 6; i++)
     {
-        printf("%d\n",vetor[i]);
+        for (int cont = 0; cont < 6; cont++){
+            printf("%d %d \n",i,cont);
+        }
     }
 }
---
+++ loop/numeros_primos_threads.py
@@ -0,0 +1,45 @@
+import threading
+import time
+
+
+
+
+
+class PRIMOS(threading.Thread):
+    def __init__(self, lock,numero,):
+        super(PRIMOS, self).__init__(daemon=True)
+        self.lock     = lock
+        self.numero = numero 
+        
+    
+    
+    def run(self):
+            cont_resto = 1
+            for divisor in range(1, self.numero):
+                if self.numero  % divisor == 0:
+                    cont_resto += 1
+            if cont_resto == 2:
+                print(f"{self.numero} é Primo")
+
+def start_threads(lock, limit,numero,):
+    threads = [PRIMOS(lock,numero,) for _ in range(limit)]
+    for t in threads: t.start()
+    return threads
+
+def stop_threads(threads):
+    for t in threads:
+        t.join()
+
+def main():
+    limit = 1
+    numero = 50423
+    try:
+        while True:
+            numero+=1
+            threads = start_threads(threading.Lock(), limit,numero,)
+            stop_threads(threads)
+    except KeyboardInterrupt:
+        print('Tchau :D')
+
+if __name__ == '__main__':
+    main()
