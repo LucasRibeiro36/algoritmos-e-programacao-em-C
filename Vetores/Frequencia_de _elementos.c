603a4d056a8ebed46322463750becba8f6f8b2e6
Lucas Ribeiro <lucasribeiro5569@gmail.com>
2022-05-18 23:45:17 -0300

    resolvi dps de 1 ano :v

--- Vetores/Frequencia_de _elementos.c
+++ Vetores/Frequencia_de _elementos.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int numero[3];
-    int frequencia[4] = {0, 0, 0, 0};
-    int numero_ant = 0;
-    for (int i = 1; i < 4; i++) {
-        printf("Digite um numero: ");
-        scanf("%d", &numero[i]);
+  int numero[3];
+  int frequencia[3] = {0, 0, 0};
+  int numero_ant = 0;
+  for (int i = 0; i < 3; i++) {
+    printf("Digite um numero: ");
+    scanf("%d", &numero[i]);
+  }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 3; j >= 0; j--) {
+      if (numero[i] == numero[j]) {
+        frequencia[j] += 1;
+      }
     }
-    for (int i = 1; i <= 4; i++) {
-        for (int j = 4; j >= 0; j--) {
-            if (numero[i] == numero[j]) {
-                frequencia[j] += 1;
-            }
-        }
+  }
+  for (int i = 0;  i <= 3; i++) {
+    if (numero[i] != numero_ant) {
+      printf("Numero %d apareceu %d\n", numero[i], frequencia[i]);
+      numero_ant = numero[i];
+    } else {
+      i++;
     }
-    for (int i = 1; i <= 3; i++) {
-        if (numero[i] != numero_ant) {
-            printf("Numero %d apareceu %d\n", numero[i], frequencia[i]);
-            numero_ant = numero[i];
-        }
-    }
-
-}  
+  }
+}
